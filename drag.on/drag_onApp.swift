ae42bbd26fc9bf3eab71e08c943bec9e56f4a54e
Assim-Genshi <assimgamre@gmail.com>
2026-02-12 16:30:26 +0100

    Initial Commit

---
+++ drag.on/drag_onApp.swift
@@ -0,0 +1,17 @@
+//
+//  drag_onApp.swift
+//  drag.on
+//
+//  Created by Assim Genshi on 12/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct drag_onApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ drag.onTests/drag_onTests.swift
@@ -0,0 +1,17 @@
+//
+//  drag_onTests.swift
+//  drag.onTests
+//
+//  Created by Assim Genshi on 12/2/2026.
+//
+
+import Testing
+@testable import drag_on
+
+struct drag_onTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
